a4ad44cb133a008339f78933ed9c9e100b5fb9bd
jeisenstat93 <51165063+jeisenstat93@users.noreply.github.com>
2026-02-23 18:54:55 +0000

    Deploying to gh-pages from @ jeisenstat93/jeisenstat93.github.io@26ed5ad28a0aebe6c04631d622050917a24c694e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jeisenstat93/website",
+    "data-repo": "jeisenstat93/jeisenstat93.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
